ad0d5c6006922b89b44244d207dc76f83cb72a26
sallyTarek <starek@eventumsolutions.com>
2023-10-23 00:05:17 +0300

    Putting directories in a config properties file instead of hard coded

--- nxtAutomationTask/src/main/java/listeners/TestListeners.java
+++ nxtAutomationTask/src/main/java/listeners/TestListeners.java
@@ -1,7 +1,9 @@
 package listeners;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
@@ -20,40 +22,58 @@ import org.testng.annotations.Parameters;
 public class TestListeners implements ITestListener {
 
 	protected WebDriver driver;
+	private static String FILE_PATH = "src/main/resources";
+	private Properties properties;
 	
-	//Plz replace path in setProperty function with real path where chromedriver exists on running
-	//Framework still needs some maintainence in matter of adjusting before and after methods
+	private void loadConfigProperties() {
+		properties = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream( FILE_PATH + File.separator + "config.properties");
+			properties.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public TestListeners() {
+		loadConfigProperties();
+	}
+
 	@BeforeClass
 	@Parameters({ "base.url" })
 	public void setUp(String baseurl) {
-		System.setProperty("webdriver.chrome.driver","D:\\SALLY\\newqpros\\CartWebsite\\nxtAutomationTask\\src\\main\\resources\\chromedriver.exe");
+		String driverPath = properties.getProperty("chromedriver.path");
+		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(baseurl);
 		driver.manage().window().maximize();
 	}
-	
+
 	@BeforeMethod
 	@Parameters({ "base.url" })
 	public void beforeMethod(String baseurl) {
 		driver.get(baseurl);
 	}
-	
-	
+
 	@AfterMethod
 	public void TestAfterMethod(ITestResult testResult) throws IOException {
 		// Taking screenshot in case of failure
-		if (testResult.getStatus() == ITestResult.FAILURE) {
-			File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			FileUtils.copyFile(scrFile, new File("errorScreenshots\\" + testResult.getName() + ".jpg"));
+		try {
+			if (testResult.getStatus() == ITestResult.FAILURE) {
+				File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+				FileUtils.copyFile(scrFile,
+						new File(properties.getProperty("screenshot.directory") + testResult.getName() + ".jpg"));
+			}
+		} catch (IOException e) {
+			System.out.println("Failed to capture or save screenshot: " + e.getMessage());
 		}
-		
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
 
 }
-
